fe72922ff90c46409b58349adf6834cd826f9ca3
user <user@com2pc54>
2018-03-08 05:37:41 -0600

    Mi primer Hola Mundo :v

---
+++ src/zczbbvjbf/Zczbbvjbf.java
@@ -0,0 +1,14 @@
+
+package zczbbvjbf;
+
+import javax.swing.JOptionPane;
+
+
+public class Zczbbvjbf {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola Mundo");
+    }
+    
+}
